b4d8bad010ad6eb7d89e134ca4371006adffe058
ussi-alok <81419246+ussi-alok@users.noreply.github.com>
2023-08-23 13:18:19 +0530

    Feature: evaluated expression through eval fn; have to find eval alternative and replace it

--- js-calculator/script.js
+++ js-calculator/script.js
@@ -2,7 +2,7 @@ let input_value = document.querySelector('.input_items');
 const btn_num_value = document.querySelectorAll('.cal_num');
 const all_btn_value = document.querySelectorAll('.cal_btn');
 let clear_input = document.querySelector('.clear_input');
-
+let eql = document.getElementById('eql');
 
 input_value.value ="";
 
@@ -12,4 +12,11 @@ all_btn_value.forEach((each_num)=>{
 
     each_num.addEventListener('click',()=>(input_value.value = input_value.value + "" + each_num.value));
     
+});
+
+
+eql.addEventListener('click',()=>{
+    // console.log(input_value.value);
+    let rm_equal = input_value.value.replace('=','');
+    return input_value.value =eval(rm_equal)
 });
